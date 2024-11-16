1db56d17c705e5c478e10974ccad99d76b614abc
Maxim Leonkin <maksimleonkin583@gmail.com>
2024-11-17 00:23:51 +0600

    Лабы

--- Алгоритмизация/1 семестр/Лабораторная 2.cs
+++ Алгоритмизация/1 семестр/Лабораторная 2.cs
@@ -1,40 +1,52 @@
 ﻿using System;
-
 class Program
 {
     static void Main()
     {
-        //Задание 1 Определить локальный максимум
+
         Console.Write("Введите количество элементов: ");
-        int n1 = Convert.ToInt32(Console.ReadLine()), lokmax = 0, a = 0, alev = 0, aprav = 0;
+        int n = Convert.ToInt32(Console.ReadLine()), lokmax = 0, a1 = 0, a2 = 0, a3 = 0, kolvo = 0, kolvo2 = 0, max1 = 0, max2 = 0;
         Console.WriteLine("Введите элементы: ");
-        a = Convert.ToInt32(Console.ReadLine());
-        aprav = Convert.ToInt32(Console.ReadLine());
-        for (int i = 2; i < n1; i++)
+        for (int i = 0; n > i; i++)
         {
-            alev = a;
-            a = aprav;
-            aprav = Convert.ToInt32(Console.ReadLine());
-            if (a > alev && a > aprav)
+            a1 = a2;
+            a2 = a3;
+            a3 = Convert.ToInt32(Console.ReadLine());
+            //Задание 1 Определить локальный минимум
+            if (a2 > a1 && a2 > a3 && i >= 2)
             {
                 lokmax++;
             }
-        }
-        Console.WriteLine($"Количество локальных максимумов: {lokmax}");
 
-        //Задание 2 Определить все ли элементы являются нечетными
-        Console.Write("Введите количество элементов: ");
-        int n2 = Convert.ToInt32(Console.ReadLine()), kolvo = 0;
-        Console.WriteLine("Введите элементы: ");
-        for (int i = 0; i < n2; i++)
-        {
-            int sim = Convert.ToInt32(Console.ReadLine());
-            if (sim % 2 != 0)
+            //Задание 2 Определить все ли элементы являются нечетными
+            if (a3 % 2 != 0)
             {
                 kolvo++;
             }
+            //Задание 3 Определить второй максимум
+            if (i == 2)
+            {
+                max1 = Math.Max(a1, Math.Max(a2, a3));
+                max2 = a1 + a2 + a3 - Math.Max(a1, Math.Max(a2, a3)) - Math.Min(a1, Math.Min(a2, a3));
+            }
+            if (a3 > max1)
+            {
+                max2 = max1;
+                max1 = a3;
+            }
+            else if (a3 > max2)
+            {
+                max2 = a3;
+            }
+            //Задание 4 Определить количество элементов, оканчивающихся на 5
+            if (Math.Abs(a3 % 10) == 5)
+            {
+                kolvo2++;
+            }
         }
-        if (kolvo==n2)
+        Console.WriteLine($"Количество локальных максимумов: {lokmax}");
+        Console.WriteLine($"Второй максимум: {max2}");
+        if (kolvo == n)
         {
             Console.WriteLine("Все элементы нечётные");
         }
@@ -43,41 +55,7 @@ class Program
             Console.WriteLine("Не все элементы нечётные");
         }
 
-        //Задание 3 Определить второй максимум
-        Console.Write("Введите количество элементов: ");
-        int n3 = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Введите элементы:");
-        int x = Convert.ToInt32(Console.ReadLine());
-        int y = Convert.ToInt32(Console.ReadLine());
-        int max1 = Math.Max(x, y);
-        int max2 = Math.Min(x, y);
-        for (int i = 2; i < n3; i++)
-        {
-            int z = Convert.ToInt32(Console.ReadLine());
-            if (z > max1)
-            {
-                max1 = z;
-            }
-            else if (z > max2)
-            {
-                max2 = z;
-            }
-        }
-        Console.WriteLine($"Второй максимум: {max2}");
-
-        //Задание 4 Определить количество элементов, оканчивающихся на 5
-        Console.Write("Введите количество элементов: ");
-        int n4 = Convert.ToInt32(Console.ReadLine()), kolvo2 = 0;
-        Console.WriteLine("Введите элементы: ");
-        for (int i = 0; i < n4; i++)
-        {
-            int sim = Convert.ToInt32(Console.ReadLine());
-            if (Math.Abs(sim % 10) == 5)
-            {
-                kolvo2++;
-            }
-        }
-        Console.WriteLine($"Количество: {kolvo2}");
+        Console.WriteLine($"Количество элементов, оканчивающихся на 5: {kolvo2}");
     }
 }
 
--- Алгоритмизация/1 семестр/Лабораторная 5.cs
+++ Алгоритмизация/1 семестр/Лабораторная 5.cs
@@ -4,16 +4,17 @@ class Program
 {
     static void Main()
     {
-        //Задание 1 Определить кол-во элементов, оканчивающихся на 3
+        
         Console.Write("Введите количество элементов массива: ");
-        int n1 = Convert.ToInt32(Console.ReadLine()), kolvo = 0;
-        int[] x = new int[n1];
+        int n = Convert.ToInt32(Console.ReadLine()), kolvo = 0, kolvo2 = 0;
+        int[] x = new int[n];
         Console.WriteLine("Введите элементы: ");
-        for (int i = 0; n1 > i; i++)
+        for (int i = 0; n > i; i++)
         {
             x[i] = Convert.ToInt32(Console.ReadLine());
         }
-        for (int i = 0; n1 > i; i++)
+        //Задание 1 Определить кол-во элементов, оканчивающихся на 3
+        for (int i = 0; n > i; i++)
         {
             if (Math.Abs(x[i] % 10) == 3)
             {
@@ -23,24 +24,16 @@ class Program
         Console.WriteLine($"Количество элементов, оканчивающихся на 3: {kolvo}");
 
         //Задание 2 Определить, являются ли элементы массива равномерно возрастающей последовательностью
-        Console.Write("Введите количество элементов массива: ");
-        int n2 = Convert.ToInt32(Console.ReadLine()), kolvo2 = 0;
-        int[] x2 = new int[n2];
-        Console.WriteLine("Введите элементы: ");
-        for (int i = 0; n2 > i; i++)
+        int razn = x[1] - x[0];
+        for (int i = 1; n > i; i++)
         {
-            x2[i] = Convert.ToInt32(Console.ReadLine());
-        }
-        int razn = x2[1] - x2[0];
-        for (int i = 1; n2 > i; i++)
-        {
-            if (razn == x2[i] - x2[i - 1] && razn > 0)
+            if (razn == x[i] - x[i - 1] && razn > 0)
             {
                 kolvo2++;
             }
 
         }
-        if (kolvo2 == n2 - 1)
+        if (kolvo2 == n - 1)
         {
             Console.WriteLine("Элементы являются равномерно возрастающей последовательностью");
         }
@@ -48,42 +41,33 @@ class Program
         {
             Console.WriteLine("Элементы не являются равномерно возрастающей последовательностью");
         }
-
         //Задание 3 Поменять местами максимальный и минимальный элементы массива
-        Console.Write("Введите количество элементов массива: ");
-        int n3 = Convert.ToInt32(Console.ReadLine());
-        int[] x3 = new int[n3];
-        Console.WriteLine("Введите элементы: ");
-        for (int i = 0; n3 > i; i++)
+        int maxi = x[0], mini = x[0];
+        for (int i = 0; n > i; i++)
         {
-            x3[i] = Convert.ToInt32(Console.ReadLine());
-        }
-        int maxi = x3[0], mini = x3[0];
-        for (int i = 0; n3 > i; i++)
-        {
-            if (x3[i] > maxi)
+            if (x[i] > maxi)
             {
-                maxi = x3[i];
+                maxi = x[i];
             }
-            if (x3[i] < mini)
+            if (x[i] < mini)
             {
-                mini = x3[i];
+                mini = x[i];
             }
         }
-        for (int i = 0; n3 > i; i++)
+        for (int i = 0; n > i; i++)
         {
-            if (x3[i] == maxi)
+            if (x[i] == maxi)
             {
-                x3[i] = mini;
+                x[i] = mini;
             }
-            else if (x3[i] == mini)
+            else if (x[i] == mini)
             {
-                x3[i] = maxi;
+                x[i] = maxi;
             }
         }
-        for (int i = 0; n3 > i; i++)
+        for (int i = 0; n > i; i++)
         {
-            Console.WriteLine(x3[i]);
+            Console.WriteLine(x[i]);
         }
     }
 }
