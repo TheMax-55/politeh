ab63b7170793c6b1cb66a3009d7a1737a65f9318
Maxim Leonkin <maksimleonkin583@gmail.com>
2025-03-01 13:52:41 +0600

    Create Лабораторная5.cs

---
+++ Алгоритмизация/2 семестр/Лабораторная5.cs
@@ -0,0 +1,44 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        HashSet<string> set = new HashSet<string>();
+        Console.Write("Введите элементы последовательности: ");
+        string[] s = Console.ReadLine().Split(' ');
+        //Задание 1
+        foreach (string sym in s)
+        {
+            set.Add(sym);
+        }
+        Console.Write("Последовательность составлена из данных элементов: ");
+        foreach (string sym in set)
+        {
+            Console.Write($"{sym} ");
+        }
+        //Задание 2
+        int count = 0;
+        string repeated = "";
+        foreach (string currsym in set)
+        {
+            int currcount = 0;
+            foreach (string sym in s)
+            {
+                if (currsym == sym)
+                {
+                    currcount++;
+                }
+            }
+            if (currcount > count)
+            {
+                count = currcount;
+                repeated = currsym + " ";
+            }
+            else if (currcount == count)
+            {
+                repeated = repeated + currsym + " ";
+            }
+        }
+        Console.Write($"\nБольше всего повторялись: {repeated}");
+    }
+}
