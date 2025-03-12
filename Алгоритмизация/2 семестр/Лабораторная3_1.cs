5dc2b7ad9dccc619a6d54f21b76ab3ea1b24882e
Maxim Leonkin <maksimleonkin583@gmail.com>
2025-03-12 09:54:07 +0600

    Create Лабораторная3_1.cs

---
+++ Алгоритмизация/2 семестр/Лабораторная3_1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics.SymbolStore;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите последовательность: ");
+        string s = Console.ReadLine();
+        bool flag = true;
+        char sym2;
+        Stack<char> st = new Stack<char>();
+        foreach (char sym in s)
+        {
+            
+                if (sym == ')' || sym == ']' || sym == '}')
+                {
+                    try
+                    {
+                        sym2 = st.Pop();
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        Console.WriteLine("Ошибка.");
+                        flag = false;
+                        break;
+                    }
+                    if (!((sym == ')' && sym2 =='(') || (sym == ']' && sym2 == '[') || (sym == '}' && sym2 == '{')))
+                        {
+                            Console.WriteLine("Ошибка.");
+                            flag = false;
+                            break;
+                        }
+                    }
+                else if (sym == '(' || sym == '[' || sym == '{')
+                {
+                    st.Push(sym);
+                }
+            
+        }
+        if (flag)
+        {
+            Console.WriteLine("Скобки расставлены верно.");
+        }
+    }
+}
