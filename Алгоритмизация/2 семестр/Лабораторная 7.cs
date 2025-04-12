43680407dae96ca4b7ca5e0a791a167bd59a3099
Maxim Leonkin <maksimleonkin583@gmail.com>
2025-04-12 20:51:24 +0600

    Create Лабораторная 7.cs

---
+++ Алгоритмизация/2 семестр/Лабораторная 7.cs
@@ -0,0 +1,53 @@
+﻿using System;
+class Coordinates
+{
+    public int x { get; set; }
+    public int y { get; set; }
+    public Coordinates(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
+delegate void OutOfRangeHandler();
+class OutOfRangeEvent
+{
+    public event OutOfRangeHandler Notify;
+    public void OutOfRangeCheck()
+    {
+        if (Notify != null)
+        {
+            Notify();
+        }
+    }
+}
+
+class Program
+{
+    static void Handler()
+    {
+        Console.WriteLine("Точка вышла за пределы поля.");
+    }
+    static void Main()
+    {
+        Random coords = new Random();
+
+        Coordinates leftAngle = new Coordinates(0, 100);
+        Coordinates rightAngle = new Coordinates(100, 0);
+        Coordinates point = new Coordinates(50, 50);
+        
+        OutOfRangeEvent evt = new OutOfRangeEvent();
+        evt.Notify += Handler;
+
+        while (1 > 0) 
+        { 
+            point.x += coords.Next(-50,50);
+            point.y += coords.Next(-50,50);
+            if (leftAngle.x > point.x || rightAngle.x < point.x || leftAngle.y < point.y || rightAngle.y > point.y)
+            {
+                evt.OutOfRangeCheck();
+                break;
+            }
+        }
+    }
+}
